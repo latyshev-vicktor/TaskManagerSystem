9f0b96b32c148305c85e3bf4ca2f0573922852ff
latyshev.vicktor <latyshev.vicktor2019@gmailcom>
2025-03-31 17:38:20 +0500

    добавил try catch

--- AuthenticationService.Application/Consumers/SendEmailByCreatedNewUserConsumer.cs
+++ AuthenticationService.Application/Consumers/SendEmailByCreatedNewUserConsumer.cs
@@ -10,27 +10,34 @@ namespace AuthenticationService.Application.Consumers
     {
         public async Task Consume(ConsumeContext<CreatedNewUser> context)
         {
-            using var smtpClient = new SmtpClient
+            try
             {
-                Host = configuration["SmptSettings:Host"],
-                Port = int.Parse(configuration["SmptSettings:Port"]),
-                Credentials = new NetworkCredential(configuration["SmptSettings:Username"], configuration["SmptSettings:Password"]),
-                EnableSsl = true
-            };
+                using var smtpClient = new SmtpClient
+                {
+                    Host = configuration["SmptSettings:Host"],
+                    Port = int.Parse(configuration["SmptSettings:Port"]),
+                    Credentials = new NetworkCredential(configuration["SmptSettings:Username"], configuration["SmptSettings:Password"]),
+                    EnableSsl = true
+                };
 
-            var body = $"Здравствуйте, {context.Message.FirstName} {context.Message.LastName}. Спасибо большое что выбрали нашу систему по планированию задач. Надеемся вы хороший котик и будете лапочкой. Всего наилучшего";
+                var body = $"Здравствуйте, {context.Message.FirstName} {context.Message.LastName}. Спасибо большое что выбрали нашу систему по планированию задач. Надеемся вы хороший котик и будете лапочкой. Всего наилучшего";
 
-            var mailMassage = new MailMessage
+                var mailMassage = new MailMessage
+                {
+                    From = new MailAddress(configuration["SmptSettings:Username"], "Task Management"),
+                    Subject = "Создание нового пользователя",
+                    Body = body,
+                    IsBodyHtml = true
+                };
+
+                mailMassage.To.Add(context.Message.Email);
+
+                await smtpClient.SendMailAsync(mailMassage);
+            }
+            catch(Exception ex)
             {
-                From = new MailAddress(configuration["SmptSettings:Username"], "Task Management"),
-                Subject = "Создание нового пользователя",
-                Body = body,
-                IsBodyHtml = true
-            };
-
-            mailMassage.To.Add(context.Message.Email);
-
-            await smtpClient.SendMailAsync(mailMassage);
+                await context.Redeliver(TimeSpan.FromMinutes(1));
+            }
         }
     }
 }
