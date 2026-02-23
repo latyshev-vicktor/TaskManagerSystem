e232e54a5bb6b8a094ac3c6ce8828ac4eee9d9f0
latyshev.vicktor <latyshev.vicktor2019@gmailcom>
2026-02-23 15:23:14 +0500

    Добавил ограничение

--- AnalyticsService.Application/Consumers/DeleteTaskConsumer.cs
+++ AnalyticsService.Application/Consumers/DeleteTaskConsumer.cs
@@ -1,4 +1,5 @@
-﻿using AnalyticsService.DataAccess.Postgres;
+﻿using AnalyticsService.Application.Interfaces.Services;
+using AnalyticsService.DataAccess.Postgres;
 using AnalyticsService.Domain.Entities;
 using AnalyticsService.Domain.Repositories;
 using DnsClient.Internal;
@@ -13,54 +14,60 @@ namespace AnalyticsService.Application.Consumers
         ISprintTaskAnalyticsRepository sprintTaskAnalyticsRepository,
         ISprintAnalitycsRepository sprintAnalitycsRepository,
         AnalyticsDbContext dbContext,
-        ILogger<DeleteTaskConsumer> logger) : IConsumer<DeleteTaskEvent>
+        ILogger<DeleteTaskConsumer> logger,
+        ITaskQueueService taskQueueService) : IConsumer<DeleteTaskEvent>
     {
         public async Task Consume(ConsumeContext<DeleteTaskEvent> context)
         {
-            using var transaction = await dbContext.Database.BeginTransactionAsync(context.CancellationToken);
             var contractMessage = context.Message;
+            var key = $"sprintId-{contractMessage.TaskId}";
 
-            try
+            await taskQueueService.Execute(key, async () =>
             {
-                var existEvent = await dbContext.MessageConsumers.AnyAsync(
-                    x => x.EventId == contractMessage.EventId
-                    && x.ConsumerName == nameof(DeleteTaskConsumer),
+                using var transaction = await dbContext.Database.BeginTransactionAsync(context.CancellationToken);
+
+                try
+                {
+                    var existEvent = await dbContext.MessageConsumers.AnyAsync(
+                        x => x.EventId == contractMessage.EventId
+                        && x.ConsumerName == nameof(DeleteTaskConsumer),
+                        cancellationToken: context.CancellationToken);
+
+                    if (existEvent)
+                    {
+                        logger.LogInformation("Событие DeleteTaskConsumer с EventId [{eventId}] уже обработано", context.Message.EventId);
+                        return;
+                    }
+
+                    var task = await sprintTaskAnalyticsRepository.GetByTask(contractMessage.TaskId)
+                        ?? throw new ApplicationException($"Не найдена задача по Id {contractMessage.TaskId}");
+
+                    var linkagesSprint = await sprintAnalitycsRepository.GetBySprintId(task.SprintId)
+                        ?? throw new ApplicationException($"Не найден спринт, привязанный к задаче с Id {task.TaskId}");
+
+                    await sprintTaskAnalyticsRepository.Delete(contractMessage.TaskId, context.CancellationToken);
+
+                    await dbContext.MessageConsumers.AddAsync(new MessageConsumerEntity
+                    {
+                        EventId = contractMessage.EventId,
+                        ConsumerName = nameof(DeleteTaskConsumer),
+                        ConsumedAtUtc = DateTime.UtcNow,
+                    },
                     cancellationToken: context.CancellationToken);
 
-                if (existEvent)
+                    await dbContext.SaveChangesAsync(context.CancellationToken);
+                    await transaction.CommitAsync();
+                }
+                catch (DbUpdateException)
                 {
                     logger.LogInformation("Событие DeleteTaskConsumer с EventId [{eventId}] уже обработано", context.Message.EventId);
-                    return;
                 }
-
-                var task = await sprintTaskAnalyticsRepository.GetByTask(contractMessage.TaskId)
-                    ?? throw new ApplicationException($"Не найдена задача по Id {contractMessage.TaskId}");
-
-                var linkagesSprint = await sprintAnalitycsRepository.GetBySprintId(task.SprintId)
-                    ?? throw new ApplicationException($"Не найден спринт, привязанный к задаче с Id {task.TaskId}");
-
-                await sprintTaskAnalyticsRepository.Delete(contractMessage.TaskId, context.CancellationToken);
-
-                await dbContext.MessageConsumers.AddAsync(new MessageConsumerEntity
+                catch (Exception ex)
                 {
-                    EventId = contractMessage.EventId,
-                    ConsumerName = nameof(DeleteTaskConsumer),
-                    ConsumedAtUtc = DateTime.UtcNow,
-                },
-                cancellationToken: context.CancellationToken);
-
-                await dbContext.SaveChangesAsync(context.CancellationToken);
-                await transaction.CommitAsync();
-            }
-            catch (DbUpdateException)
-            {
-                logger.LogInformation("Событие DeleteTaskConsumer с EventId [{eventId}] уже обработано", context.Message.EventId);
-            }
-            catch(Exception ex)
-            {
-                await transaction.RollbackAsync();
-                logger.LogError(ex, "Ошибка при обработке DeleteTaskConsumer с EventId [{eventId}]", contractMessage.EventId);
-            }
+                    await transaction.RollbackAsync();
+                    logger.LogError(ex, "Ошибка при обработке DeleteTaskConsumer с EventId [{eventId}]", contractMessage.EventId);
+                }
+            });
         }
     }
 }
